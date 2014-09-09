53ece2133c57b89e74c89cf706b1a961e4ffa574
Timo Uhlmann <timouhlmann@googlemail.com>
2014-09-09 18:18:08 +0200

    /slap <nick> [<weapon>]

---
+++ slap.js
@@ -0,0 +1,13 @@
+(function() {
+  setName('slap');
+  setDescription('Slaps other players. Never gets tired.');
+
+  send('hook_command', 'slap');
+
+  this.onMessage = function(e) {
+    var victim = e.args[0];
+    var weapon = e.args[1] || 'a large trout';
+    send(e.context, 'command', 'me', 'slaps ' + victim + ' around a bit with ' + weapon);
+    propagate(e, 'none');
+  };
+}).call(this);
